fc453f86a66871e80da81f88e5880b21a69928dd
98prabowo <58059518+98prabowo@users.noreply.github.com>
2025-02-01 03:10:11 +0700

    feat(utils): create `FileOpener` interface to inject service upload image

--- internal/utils/rwutil/rw_util.go
+++ internal/utils/rwutil/rw_util.go
@@ -1,9 +1,23 @@
 package rwutil
 
-import "io"
+import (
+	"io"
+	"mime/multipart"
+)
 
 type ReadFile func(r io.Reader) ([]byte, error)
 
 type Reader interface {
 	ReadAll(r io.Reader) ([]byte, error)
+}
+
+type FileOpener interface {
+	Open() (multipart.File, error)
+}
+
+type FileData interface {
+	io.Reader
+	io.ReaderAt
+	io.Seeker
+	io.Closer
 }
