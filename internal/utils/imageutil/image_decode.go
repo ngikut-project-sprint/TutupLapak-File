d9fb84ba9419bc4d0e45e9d31aac13dff2af5355
98prabowo <58059518+98prabowo@users.noreply.github.com>
2025-02-01 03:10:41 +0700

    fix(utils): image decoder decode method arguments

--- internal/utils/imageutil/image_decode.go
+++ internal/utils/imageutil/image_decode.go
@@ -8,5 +8,5 @@ import (
 type DecodeImage func(r io.Reader) (image.Image, error)
 
 type ImageDecoder interface {
-	Decode(r io.Reader) (image.Image, string, error)
+	Decode(r io.Reader) (image.Image, error)
 }
