e37da5ff2dcd3f424079887b41faebfd92b5fa93
98prabowo <58059518+98prabowo@users.noreply.github.com>
2025-01-31 21:19:04 +0700

    feat(service): create file service

---
+++ internal/service/interface.go
@@ -0,0 +1,14 @@
+package service
+
+import (
+	"context"
+	"mime/multipart"
+
+	"github.com/98prabowo/tutuplapak-file/internal/model"
+)
+
+type FileService interface {
+	UploadFile(ctx context.Context, file *multipart.FileHeader, fileName string, completion chan model.Completion)
+	GenerateThumbnail(ctx context.Context, file *multipart.FileHeader, fileName string, completion chan model.Completion)
+	AddFile(ctx context.Context, fileURI string, fileThumbnailURI string) (uint, error)
+}
